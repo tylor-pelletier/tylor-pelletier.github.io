b93deb2879bfcf72f7610aea9b43cac0f3e5a26a
Tylor Pelletier <tylorpelletier@Tylors-MacBook-Pro.local>
2018-08-03 10:42:54 -0500

    checkout new style

---
+++ website.js
@@ -0,0 +1,102 @@
+// <!DOCTYPE html>
+// <html lang="en">
+//
+//     <head>
+//
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1">
+//
+//     <title>Tylor Pelletier</title>
+//
+// <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
+//     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.2/slate/bootstrap.min.css" integrity="sha384-fSVTHAcdTrW2AQFxwcv4yRDyQdnteeIOLhHrrKIO4xTLK+JrplIISHbIHvfcYEKK" crossorigin="anonymous">
+//     <link rel="stylesheet" href="website.css">
+//     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fredoka+One">
+//
+//     </head>
+//
+//     <body>
+//
+//     <header>
+//
+//     <nav class="navbar fixed-top navbar-dark bg-primary p-0">
+//     <div class="col-4 p-0">
+//     <!--LINK TO MY ABOUT ON PAGE-->
+// <a class="navbar-brand" href="#"><h2 class="light-blue-font-color">Tylor Pelletier</h2></a>
+// </div>
+// <div class="col-4 d-none d-md-block text-center">
+//     ADD LOGO HERE
+// </div>
+// <div class="col-4 text-right">
+//     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+//     <span class="navbar-toggler-icon"></span>
+//     </button>
+//     </div>
+//
+//     <div class="collapse navbar-collapse text-center" id="navbarColor01">
+//     <ul class="navbar-nav mr-auto">
+//     <li class="nav-item">
+//     <a class="nav-link" href="#"><span class="purple-font-color">Experience</span></a>
+// </li>
+// <li class="nav-item">
+//     <a class="nav-link" href="#"><span class="purple-font-color">Social Media</span></a>
+// </li>
+// <li class="nav-item">
+//     <a class="nav-link" href="#"><span class="purple-font-color">Contact</span></a>
+// </li>
+// <li class="nav-item">
+//     <a class="nav-link" href="#"><span class="purple-font-color">About</span></a>
+// </li>
+// </ul>
+// </div>
+// </nav>
+//
+// </header>
+//
+// <div class="parallax"></div>
+//
+//     <div class="paragraph">
+//
+//     <h1 class="purple-font-color">The world is filled with millions of opportunities waiting to be discovered</h1>
+//
+// </div>
+//
+// <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+//     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+//     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+//     <script src="website.js"></script>
+//
+//     </body>
+//
+//     </html>
+//
+//
+//
+// body {
+//     font-family: 'Fredoka One', cursive;
+// }
+//
+// .parallax {
+//     /* The image used */
+//     background-image: url("coral.jpeg");
+//
+//     min-height: 800px;
+//
+//     background-attachment: fixed;
+//     background-position: center;
+//     background-repeat: no-repeat;
+//     background-size: cover;
+// }
+//
+// .paragraph {
+//     height:900px;
+//     font-size:36px
+// }
+//
+// .purple-font-color {
+//     color: #814DF6;
+// }
+//
+// .light-blue-font-color {
+//     color: #c9f0ff
+// }
