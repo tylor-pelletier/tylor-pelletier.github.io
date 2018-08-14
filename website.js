80988bba32c9ebd0d5969c1895b79d39ea68614c
Tylor Pelletier <tylorpelletier@Tylors-MBP.attlocal.net>
2018-08-13 20:13:46 -0500

    added scrolling feature

--- website.js
+++ website.js
@@ -12,3 +12,13 @@ $(function() {
         }, 1000);
     });
 });
+
+function scrollNav() {
+    $('a').click(function(){ $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 160
+    }, 400);
+        return false;
+    });
+    $('.scrollTop a').scrollTop();
+}
+scrollNav();
