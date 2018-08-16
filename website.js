34af5718c408dd6777a97eb31a4e8f179ade9646
Tylor Pelletier <tylorpelletier@Tylors-MacBook-Pro.local>
2018-08-16 12:06:33 -0500

    check timing

--- website.js
+++ website.js
@@ -21,7 +21,7 @@
             ovrl.style.opacity = 0;
             setTimeout(function(){
                 ovrl.style.display = "none";
-            }, 1200);
+            }, 2000);
         }
         for(var i=0; i<tot; i++) {
             var tImg     = new Image();
